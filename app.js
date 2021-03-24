25da650e385b747c1994b045e088845878c0ffb8
Shurik <Skaramush80@gmail.com>
2021-03-24 18:20:54 +0200

    file and directory list full tmp

--- app.js
+++ app.js
@@ -3,9 +3,26 @@ let getString = function () {
   return process.argv[2];
 }
 const fs = require("fs");
-const items = fs.readdirSync(getString());
 
-for ( let i = 0; i <= items.length; i++){
-  console.log(items[i]);
+function findFiles(folderName) {
+  const items = fs.readdirSync(folderName, { withFileTypes: true });
+  items.forEach((item) => {
+    if (item.isDirectory()) {
+       findFiles(`${folderName}/${item.name}`);
+    } else {
+      console.log(`folder: ${folderName} File: ${item.name} `);
+    }
+  });
 }
+
+findFiles(getString());
+
+//const items = fs.readdirSync(getString(), { withFileTypes: true });
+//for (let item of items) {
+  //const type = item.isDirectory() ? "folder" : "file";
+  //console.log(`${item.name}: ${type}`);
+
+//}
+
+
 
