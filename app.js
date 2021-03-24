182955b0e981fca379725dffc24472bdb1a6030c
Shurik <Skaramush80@gmail.com>
2021-03-24 16:08:08 +0200

    file and directory list easy

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 let getString = function () {
   return process.argv[2];
 }
+const fs = require("fs");
+const items = fs.readdirSync(getString());
 
-let testStr = "hello, " + getString();
-console.log(testStr);
+for ( let i = 0; i <= items.length; i++){
+  console.log(items[i]);
+}
+
